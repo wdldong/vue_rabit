c3b4f66892ffb0cfd3e6b0653859bf5a57c18f01
WDL <2465797130@qq.com>
2024-02-21 17:52:55 +0800

    主题色的定制

--- vite.config.js
+++ vite.config.js
@@ -10,17 +10,32 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
   plugins: [
     vue(),
-    // ...
+    // 配置element plus
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
+    // 1、需要替换的地方开始
     Components({
-      resolvers: [ElementPlusResolver()],
-    }),
+      // 用sass配置主题色
+      resolvers: [ElementPlusResolver({ importStyle: "sass" })],
+    })
+    // 1、需要替换的地方结束
   ],
   resolve: {
+    // 实际的路径转换
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  // 用sass配置主题色
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
