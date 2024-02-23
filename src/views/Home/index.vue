f3f513fa6559dbf529da3d0984e57050c4f063d2
WDL <2465797130@qq.com>
2024-02-23 16:17:29 +0800

    HomeCategory分类组件的布局以及渲染数据

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,8 +1,16 @@
+<script setup>
+import HomeBanner from '@/views/Home/components/HomeBanner.vue'
+import HomeCategory from '@/views/Home/components/HomeCategory.vue'
+import HomeHot from '@/views/Home/components/HomeHot.vue'
+import HomeList from '@/views/Home/components/HomeList.vue'
+import HomeNew from '@/views/Home/components/HomeNew.vue'
+</script>
 <template>
-    <div class="test">Home</div>
+    <div class="container">
+        <HomeCategory />
+        <HomeBanner />
+    </div>
+    <HomeNew />
+    <HomeHot />
+    <HomeList />
 </template>
-<style lang="scss" scoped>
-    .test {
-        color: $priceColor;
-    }
-</style>
