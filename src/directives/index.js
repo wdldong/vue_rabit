ef85eacb2466b5de664bd6e962e611011c0418fc
WDL <2465797130@qq.com>
2024-02-28 15:58:02 +0800

    图片懒加载指令优化以及优化内存浪费问题

---
+++ src/directives/index.js
@@ -0,0 +1,24 @@
+// 图片懒加载插件
+import { useIntersectionObserver } from '@vueuse/core'
+
+export const lazyPlugin = {
+    install(app) {
+        app.directive('img-lazy', {
+            mounted(el, binding) {
+                // el指的是绑定的那个元素 img
+                // binging 指令等号后面绑定的表达式的值  图片url
+                console.log(el, binding.value);
+                const { stop } = useIntersectionObserver(
+                    el,
+                    ([{ isIntersecting }]) => {
+                        console.log(isIntersecting);
+                        if (isIntersecting) {
+                            el.src = binding.value
+                            stop()
+                        }
+                    },
+                )
+            }
+        })
+    }
+}
--- src/main.js
+++ src/main.js
@@ -4,27 +4,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import { useIntersectionObserver } from '@vueuse/core'
-
+import { lazyPlugin } from '@/directives/index'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
-app.directive('img-lazy', {
-    mounted(el, binding) {
-        // el指的是绑定的那个元素 img
-        // binging 指令等号后面绑定的表达式的值  图片url
-        console.log(el, binding.value);
-        useIntersectionObserver(
-            el,
-            ([{ isIntersecting }]) => {
-                console.log(isIntersecting);
-                if (isIntersecting) {
-                    el.src = binding.value
-                }
-            },
-        )
-    }
-})
+app.use(lazyPlugin)
