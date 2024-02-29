2157c01a6aca859e59374b699bcb11405ac77261
WDL <2465797130@qq.com>
2024-02-29 10:11:03 +0800

    一级路由配置和详细页面

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -17,7 +17,7 @@ const categoryStore = useCategoryStore()
       <ul class="app-header-nav">
         <li class="home"><RouterLink to="/">首页</RouterLink></li>
         <li class="home" v-for="item in categoryStore.cateGoryList" :key="item.id">
-          <RouterLink to="/">{{ item.name }}</RouterLink>
+          <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
         </li>
 
       </ul>
