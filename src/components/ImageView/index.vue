d6d0b15bbd2caa4c46c60e5d589f9587e0d5a69f
WDL <2465797130@qq.com>
2024-03-08 17:15:19 +0800

    放大镜总体功能实现

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -18,8 +18,12 @@ const mouseEvent = (i) => {
 // 1、获取鼠标相对位置
 const target = ref(null)
 
+// 滑块的坐标
 const left = ref(0)
 const top = ref(0)
+// 大图的坐标
+const positionX = ref(0)
+const positionY = ref(0)
 const { elementX,elementY,isOutside} = useMouseInElement(target)
 // 2、控制滑块跟随鼠标移动（watch监听elementX,elementY的变化，一旦变化，重新设置left/top）
 watch([elementX,elementY],() => {
@@ -39,7 +43,9 @@ watch([elementX,elementY],() => {
   if(elementX.value > 300) { left.value = 200}
   if(elementY.value < 100) { top.value = 0}
   if(elementY.value > 300) { top.value = 200}
-  
+  // 控制大图的显示
+  positionX.value = - left.value * 2
+  positionY.value = - top.value * 2
 })
 
 </script>
@@ -49,7 +55,7 @@ watch([elementX,elementY],() => {
     <div class="middle" ref="target">
       <img :src="imageList[activeType]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
+      <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -58,13 +64,13 @@ watch([elementX,elementY],() => {
       </li>
     </ul>
     <!-- 放大镜大图 -->
-    <div class="large" :style="[
+    <div class="large" v-show="!isOutside" :style="[
       {
         backgroundImage: `url(${imageList[0]})`,
-        backgroundPositionX: `0px`,
-        backgroundPositionY: `0px`,
+        backgroundPositionX: `${positionX}px`,
+        backgroundPositionY: `${positionY}px`,
       },
-    ]" v-show="false"></div>
+    ]"></div>
   </div>
 </template>
 
