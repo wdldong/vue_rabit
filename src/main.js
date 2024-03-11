6fb256e2ed149903a4eff2f54da3df7a7305ee24
WDL <2465797130@qq.com>
2024-03-11 10:21:47 +0800

    优化全局组件插件化

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+// 引入图片懒加载插件
 import { lazyPlugin } from '@/directives/index'
+// 引入全局组件插件 
+import { componentPlugin } from '@/components/index'
 
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
 app.use(lazyPlugin)
+app.use(componentPlugin)
--- src/views/Details/index.vue
+++ src/views/Details/index.vue
@@ -3,8 +3,8 @@ import {getDetail} from '@/apis/detail'
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import DetailHot from './components/DetailHot.vue';
-import ImageView from '@/components/ImageView/index.vue'
-import XtxSku from '@/components/XtxSku/index.vue'
+// import ImageView from '@/components/ImageView/index.vue'
+// import XtxSku from '@/components/XtxSku/index.vue'
 
 const route = useRoute()
 const goods = ref({})
@@ -16,6 +16,7 @@ const getGoods = async () => {
 onMounted(() => {
   getGoods()
 })
+// sku组件的事件
 const skuChange = (sku) => {
   console.log(sku);
 }
