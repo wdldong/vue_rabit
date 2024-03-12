6d70c2b5ca6ad21607847ce3202a402a6a6b2589
WDL <2465797130@qq.com>
2024-03-12 16:38:52 +0800

    自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -4,7 +4,8 @@ import { ref } from 'vue';
 // 1.准备表单对象
 const form = ref({
   account: '',
-  password: ''
+  password: '',
+  aggre: true
 })
 
 // 2.准备规则对象
@@ -15,6 +16,19 @@ const rules = {
   password: [
     { required: true, message: '密码不能为空', trigger: 'blur'},
     { min: 6, max: 14, message: '密码长度为6-14位字符', trigger: 'blur'}
+  ],
+  aggre: [
+    {
+      validator: (rule,value,callback) => {
+        // console.log(value);
+        if(value) {
+          callback()
+        }else {
+          callback(new Error('请勾选协议'))
+        }
+
+      }
+    }
   ]
 }
 
@@ -50,8 +64,8 @@ const rules = {
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password" />
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item prop="aggre" label-width="22px">
+                <el-checkbox  size="large" v-model="form.aggre">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
