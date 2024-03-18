df4e2fa67dea1adf871bac17a9ca07624d028893
WDL <2465797130@qq.com>
2024-03-18 15:59:33 +0800

    请求拦截器携带token

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@
 import axios from "axios"
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import { useUserStore } from "@/stores/user"
 // 封装axios
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -10,6 +11,12 @@ const httpInstance = axios.create({
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+    // 从pinia中获取token数据
+    const userStore = useUserStore()
+    const token = userStore.userInfo.token
+    if(token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
     return config
 }, e => Promise.reject(e))
 
